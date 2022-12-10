58f52c6e94f64f1ddd5bf9589fd48d2bb9f4899a
Алмас Синев <114406236+andromeda-school@users.noreply.github.com>
2022-12-10 13:55:26 +0300

    Работа с файлами 10.12.2022

--- login.py
+++ login.py
@@ -2,11 +2,14 @@
 DATABASE_NAME = "database.txt"
 
 def append_user(login, password):
-    file1 = open(DATABASE_NAME, "a")
-    line = "\nlogin : " + login + ", password : " + password
-    file1.write(line)
-    print(f"Добавлен новый пользователь '{login}'")
-    file1.close()
+    if not find_login(login):
+        file1 = open(DATABASE_NAME, "a")
+        line = "\nlogin : " + login + ", password : " + password
+        file1.write(line)
+        print(f"Добавлен новый пользователь '{login}'")
+        file1.close()
+    else:
+        print("Этот логин уже занят!")
 
 def read_users():
     file1 = open(DATABASE_NAME, "r")
@@ -26,8 +29,40 @@ def read_users():
     file1.close()
 
 
-read_users()
-append_user("robot7", "7777")
-append_user("lenka1212", "4545")
-read_users()
+def find_login(login):
+    file1 = open(DATABASE_NAME, "r")
+    arr = file1.readlines()
+    for acc in arr:
+        user_login = acc.split(",")[0].split(":")[1].strip()
+        if user_login == login:
+            return True
+    file1.close()
+    return False
 
+def check_password(login, password):
+    file1 = open(DATABASE_NAME, "r")
+    arr = file1.readlines()
+    for acc in arr:
+        user_login = acc.split(",")[0].split(":")[1].strip()
+        if user_login == login:
+            user_password = acc.split(",")[1].split(":")[1].strip()
+            if password == user_password:
+                print("Успешно! Добро пожаловать!")
+                file1.close()
+                return True
+            else:
+                print("Пароль неверный.")
+                file1.close()
+                return False
+    file1.close()
+    return False
+
+
+login = input("Введите логин: ")
+while not find_login(login):
+    print("Такого пользователя нет в системе.\n")
+    login = input("Введите логин: ")
+
+password = input("Введите пароль: ")
+while not check_password(login, password):
+    password = input("Введите пароль: ")
